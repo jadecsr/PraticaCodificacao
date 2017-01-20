20e6e208b2db786ae8fee53a6608d71e8b4b1701
Davi Nascimento <dno@icomp.ufam.edu.br>
2017-01-20 19:03:18 -0400

    primeiro commit

---
+++ EquipeE/Aeroporto.java
@@ -0,0 +1,19 @@
+
+
+public class Aeroporto {
+
+    public static void main(String[] args) {
+        int procurado;
+        int[] vetor;
+
+        int[] vetor = new int[100];
+
+        for (int i = 0;i < vetor.length ;i++ ) {
+            if (procurado == vetor[i]){
+                System.out.println();
+            }
+        }
+
+    }
+
+}
